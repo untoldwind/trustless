5d1a045fd2fd74cc1f8dcbde1eaa7f3236ae89e9
Bodo Junglas <bodo.junglas@leanovate.de>
2017-04-05 16:18:49 +0200

    Fix defaults for unix

--- config/defaults_linux.go
+++ config/defaults_linux.go
@@ -1,12 +1,11 @@
 package config
 
 import (
-	"net/url"
 	"os"
 	"path/filepath"
 )
 
 // DefaultStoreURL gets the default location of the store
 func DefaultStoreURL() string {
-	return "file://" + url.PathEscape(filepath.Join(os.Getenv("HOME"), ".trustless_store"))
+	return "file://" + filepath.Join(os.Getenv("HOME"), ".trustless_store")
 }
