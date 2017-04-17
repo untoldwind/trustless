1050997362e761aca201dd01ed021f3204d5a7f7
Bodo Junglas <bodo.junglas@leanovate.de>
2017-04-17 16:46:55 +0200

    Fix json

--- api/identity.go
+++ api/identity.go
@@ -3,5 +3,5 @@ package api
 // Identity contains information about anyone allowed to access a trust store
 type Identity struct {
 	Name  string `json:"name"`
-	Email string `json:"Email"`
+	Email string `json:"email"`
 }
