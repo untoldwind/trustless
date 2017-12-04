b677457dd8c03fe57d85c92829568fcb2348e5cf
Bodo Junglas <untoldwind@googlemail.com>
2017-12-04 07:28:16 +0100

    Basic export to keepass

--- commands/export.go
+++ commands/export.go
@@ -4,18 +4,34 @@ import (
 	"encoding/json"
 	"io"
 	"os"
+	"strings"
+
+	"github.com/tobischo/gokeepasslib"
 
 	"github.com/pkg/errors"
 
 	"github.com/untoldwind/trustless/api"
+	"github.com/untoldwind/trustless/secrets"
 	cli "gopkg.in/urfave/cli.v2"
 )
+
+var ExportFlags = struct {
+	Format string
+}{}
 
 var ExportCommand = &cli.Command{
 	Name:      "export",
 	Usage:     "Export store",
 	ArgsUsage: "[<file>]",
-	Action:    withDetailedErrors(exportFile),
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:        "format",
+			Usage:       "Export format (json, keepass)",
+			Value:       "json",
+			Destination: &ExportFlags.Format,
+		},
+	},
+	Action: withDetailedErrors(exportFile),
 }
 
 func exportFile(ctx *cli.Context) error {
@@ -26,23 +42,38 @@ func exportFile(ctx *cli.Context) error {
 		return err
 	}
 
-	var out io.Writer = os.Stdout
-
+	fileName := ""
 	if ctx.Args().Len() > 0 {
-		file, err := os.OpenFile(ctx.Args().Get(0), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-		if err != nil {
-			return errors.Wrapf(err, "Unable to open: %s", ctx.Args().Get(0))
-		}
-		defer file.Close()
-
-		out = file
+		fileName = ctx.Args().Get(0)
 	}
 
+	switch ExportFlags.Format {
+	case "json":
+		return exportJson(client, fileName)
+	case "keepass":
+		return exportKeepass(client, fileName)
+	default:
+		return errors.Errorf("Invalid export format: %s", ExportFlags.Format)
+	}
+}
+
+func exportJson(client secrets.Secrets, fileName string) error {
 	secrets, err := client.List(createClientContext(), api.SecretListFilter{})
 	if err != nil {
 		return err
 	}
 
+	var out io.Writer = os.Stdout
+
+	if fileName != "" {
+		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+		if err != nil {
+			return errors.Wrapf(err, "Unable to open: %s", fileName)
+		}
+		defer file.Close()
+
+		out = file
+	}
 	encoder := json.NewEncoder(out)
 	for _, entry := range secrets.Entries {
 		secret, err := client.Get(createClientContext(), entry.ID)
@@ -55,4 +86,52 @@ func exportFile(ctx *cli.Context) error {
 	}
 
 	return nil
+}
+
+func exportKeepass(client secrets.Secrets, fileName string) error {
+	database := gokeepasslib.NewDatabase()
+
+	root := gokeepasslib.NewGroup()
+	root.Name = "trustless"
+
+	secrets, err := client.List(createClientContext(), api.SecretListFilter{})
+	if err != nil {
+		return err
+	}
+	for _, entry := range secrets.Entries {
+		secret, err := client.Get(createClientContext(), entry.ID)
+		if err != nil {
+			return err
+		}
+		entry := gokeepasslib.NewEntry()
+		entry.Values = []gokeepasslib.ValueData{
+			{Key: "Title", Value: gokeepasslib.V{Content: secret.Current.Name}},
+		}
+		entry.Tags = strings.Join(secret.Current.Tags, ",")
+		root.Entries = append(root.Entries, entry)
+	}
+
+	database.Content = &gokeepasslib.DBContent{
+		Meta: gokeepasslib.NewMetaData(),
+		Root: &gokeepasslib.RootData{
+			Groups: []gokeepasslib.Group{root},
+		},
+	}
+	database.LockProtectedEntries()
+
+	var out io.Writer = os.Stdout
+
+	if fileName != "" {
+		file, err := os.OpenFile(fileName, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+		if err != nil {
+			return errors.Wrapf(err, "Unable to open: %s", fileName)
+		}
+		defer file.Close()
+
+		out = file
+	}
+
+	encoder := gokeepasslib.NewEncoder(out)
+
+	return encoder.Encode(database)
 }
